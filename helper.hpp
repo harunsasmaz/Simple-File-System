86825ee3d6ebde796d9f725394fa8bd6136204f9
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-05-14 15:23:17 +0300

    utility functions implemented

--- helper.hpp
+++ helper.hpp
@@ -5,19 +5,20 @@ typedef struct DTEntry {
     int size; // in terms of number of occupied blocks.
 }dtentry_t;
 
-
 // returns the first block index of the first fit in directory content.
-int find_first_fit(int starting_block, int required_block);
-// returns the starting index of the next file.
-int find_next_file(int starting_position);
+int find_first_fit(int required_block);
+// checks if the contiguous space is enough to place a file
+bool seek(int starting_position, int required_block);
+// returns the id of the next file in a given range.
+int find_next_file(int starting_position, int end_position);
 // returns the required amount of blocks for a given size.
 int byte_to_block(int bytes);
 // fills the directory content array with the unique id of the file.
 int fill_directory_content(dtentry_t entry);
-// shifts left the all available blocks.
+// shifts left the all files.
 int defragment();
-// shifts a file to left by a given amount of steps.
-int move_file_to_left(dtentry_t file, int step);
+// shifts a file to left as much as it can, and returns the new starting index of file.
+int move_file_to_left(dtentry_t file);
 
 
 // required functions
---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include "stdc++.h"
+#include "helper.hpp"
+
+using namespace std;
+#define NUM_BLOCKS 32768
+#define BLOCK_SIZE 1024
+
+
+int directory_contents[NUM_BLOCKS];
+int available_blocks = NUM_BLOCKS;
+map<int, dtentry_t> DT;
+
+int main(int argc, char** argv){
+
+    fill(directory_contents, directory_contents + NUM_BLOCKS, 0);
+
+
+
+
+    return 0;
+}
+
+// byte to required amount of block conversion.
+int byte_to_block(int bytes){
+    return ceil(static_cast<float>(bytes) / static_cast<float>(BLOCK_SIZE));
+}
+
+// fill the directory content array according to the entry data.
+int fill_directory_content(dtentry_t entry){
+    int start = entry.starting_index;
+    int occupied = entry.size;
+    int id = entry.file_id;
+    fill(directory_contents+start, directory_contents+start+occupied, id);
+    return 0;
+}
+
+// find the id of the next file in given range.
+int find_next_file(int starting_position, int end_position){
+    int id = -1;
+    for(int i = starting_position; i < end_position; i++){
+        if(directory_contents[i] != 0){
+            id = directory_contents[i];
+            break;
+        }
+    }
+    return id;
+}
+
+// search a space to allocate a file
+int find_first_fit(int required_block){
+    int start = 0;
+    dtentry_t entry;
+    while(start < NUM_BLOCKS - required_block + 1){
+        // check if there is enough contiguous space
+        bool check = seek(start, required_block);
+        //if yes, then we found starting index
+        if(check) break;
+        // if not, update the start index to the end of the next file.
+        int id = find_next_file(start, start + required_block);
+        entry = DT[id];
+        start = entry.starting_index + entry.size;
+    }
+    return start;
+}
+
+// search if there is enough contiguous space in given range.
+bool seek(int starting_position, int required_block){
+    for(int i = starting_position; i < starting_position + required_block; i++){
+        if(directory_contents[i] != 0) return false;
+    }
+    return true;
+}
+
+// shifts a file as much as it can, and returns the new starting index of file.
+int move_file_to_left(dtentry_t* file){
+
+    int first = file->starting_index, size = file->size, id = file->file_id;
+    int start = first - 1;
+
+    while(start != -1 && directory_contents[start] == 0){
+
+        for(int i = 0; i < size; i++){
+            int temp = directory_contents[start + i];
+            directory_contents[start + i] = directory_contents[first + i];
+            directory_contents[first + i] = temp;
+        }
+        file->starting_index = start;
+        first--;
+        start--;
+    }
+
+    return first;
+}
+
+// for all the files in DT, shift them left. 
+int defragment(){
+
+    for(map<int, dtentry_t>::iterator it = DT.begin(); it != DT.end(); it++){
+        move_file_to_left(it->second);
+    }
+    return 0;
+}
